c5b60306b960157488e6b1621255dd679edc04a1
Michael Nugent <nugent.michael@gmail.com>
2014-03-07 12:35:57 -0500

    JSON Integration and Responsive Fixes
    - Added JSON functionality for listed event projects.
    - Additional fixes to the responsive layout on mobile devices.

--- js/scripts.js
+++ js/scripts.js
@@ -14,4 +14,96 @@ $(document).ready(function() {
   $('.flexslider').flexslider({
     animation: "slide"
   });
+
+  /* JSON Functionality */
+  function loadJSON(){
+   var data_file = "data.json";
+   var http_request = new XMLHttpRequest();
+   try{
+      // Opera 8.0+, Firefox, Chrome, Safari
+      http_request = new XMLHttpRequest();
+   }catch (e){
+      // Internet Explorer Browsers
+      try{
+         http_request = new ActiveXObject("Msxml2.XMLHTTP");
+      }catch (e) {
+         try{
+            http_request = new ActiveXObject("Microsoft.XMLHTTP");
+         }catch (e){
+            // Something went wrong
+            alert("Your browser may not support AJAX, or refresh cause something went wrong");
+            return false;
+         }
+      }
+   }
+   http_request.onreadystatechange  = function(){
+
+      // readyState == 4 means the request was successful
+      if (http_request.readyState == 4  ){
+        // Javascript function JSON.parse to parse JSON data
+        var jsonObj = JSON.parse(http_request.responseText);
+
+        // My Entire JSON Object from data.json
+        // console.log(jsonObj);
+
+        // var img = document.createElement("img");
+        // img.src = jsonObj[0].imageUrl;
+        // document.appendChild(img);
+
+        // Div to append all my results to
+        var projectWrapper = document.getElementById('projects');
+
+        // Create an Img with src attribute as the imageURL
+        var projectImage = document.createElement('img');
+        // articleImage.setAttribute('src',jsonObj[0].imageUrl);
+
+        // Article Heading
+        var projectHeading = document.createElement('h1');
+        // articleHeading.innerHTML = jsonObj[0].title;
+
+        // Append the Image to the article-feed div.
+        projectWrapper.appendChild(projectImage);
+        projectWrapper.appendChild(projectHeading);
+
+        for (var key in jsonObj) {
+
+          var newDiv = document.createElement('div');
+          // console.log(jsonObj[key]);
+
+          // Create an Img with src attribute as the imageURL
+          var projectImage = document.createElement('img');
+          projectImage.setAttribute('src',jsonObj[key].imageUrl);
+
+          // Article Heading
+          var projectHeading = document.createElement('h1');
+          projectHeading.innerHTML = jsonObj[key].title;
+
+          // Article Heading
+          var projectDescription = document.createElement('p');
+          projectDescription.innerHTML = jsonObj[key].description;
+
+          // Article Image
+          // var articleImage = document.createElement('img');
+          // var articleURL = document.createElement('a');
+          // articleURL.innerHTML = jsonObj[key].imageUrl;
+          // articleImage.setAttribute('src',jsonObj[key].imageUrl);
+          
+          var imageLink = document.createElement('a');
+          imageLink.setAttribute('href',jsonObj[key].url);
+          imageLink.appendChild(projectImage);
+
+          // Append the Image to the article-feed div.
+          newDiv.appendChild(imageLink);
+          newDiv.appendChild(projectHeading);
+          newDiv.appendChild(projectDescription);
+          projectWrapper.appendChild(newDiv);
+        }
+      }
+
+   }
+   http_request.open("GET", data_file, true);
+   http_request.send();
+}
+
+loadJSON();
 });
